ea3d5ea7f108bd50f07caa5215a8fb3bc701abae
ChrisChanAmon <157276328+ChrisChanAmon@users.noreply.github.com>
2024-02-29 21:19:51 +0800

    Commit Changes

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+AOS.init();
+
+
+AOS.init({
+  
+
+  
+  offset: 120,  
+  delay: 0,  
+  duration: 700,  
+  easing: 'ease',  
+  once: false,   
+  mirror: false, 
+  anchorPlacement: 'top-bottom',  
+
+});
+
+
+var typed = new Typed(".typing",{
+  strings:["", "Web Designer", "Web Developer", "Virtual Assistant", "SEO Specialist", "Lead Generations Expert"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
